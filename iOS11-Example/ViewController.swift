10656a68c86fecc465982cd3d3be8fce54f28378
Dragon <longfei.ren@qunar.com>
2017-10-11 17:26:24 +0800

    修改

--- iOS11-Example/ViewController.swift
+++ iOS11-Example/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("safeInset",self.view.safeAreaInsets)
+        
+        print("safeLayout", self.view.safeAreaLayoutGuide)
+
         // Do any additional setup after loading the view, typically from a nib.
         
     }
